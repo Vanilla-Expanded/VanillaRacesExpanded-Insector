a19a6a82161ba2566dc77616dcdbfbe3f4c17732
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-02-29 15:58:45 +0100

    First three genes

---
+++ 1.4/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/DefOfs/InternalDefOf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedInsector
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+        public static GeneDef VRE_InsectFlesh;
+
+        public static ThingDef Meat_Megaspider;       
+
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_Acceptable;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_Preferred;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_RequiredStrong;
+        [MayRequireIdeology]
+        public static PreceptDef Cannibalism_RequiredRavenous;
+
+
+    }
+}
