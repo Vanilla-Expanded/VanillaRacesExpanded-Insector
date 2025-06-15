f544ef6ecd45927c969a918e8a30efad2f4209e3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-15 23:07:43 +0200

    v2.0

---
+++ 1.6/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/Genelines/Metapod.cs
@@ -0,0 +1,172 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+namespace VanillaRacesExpandedInsector
+{
+    [HotSwappable]
+    public class Metapod : Building, IThingHolder
+    {
+        public ThingOwner innerContainer;
+        public Pawn Pawn => (Pawn)innerContainer.FirstOrDefault();
+        protected Effecter progressBarEffecter;
+        private Effecter wakeUpEffect;
+        public CompRefuelable compRefuelable;
+
+        public Metapod()
+        {
+            innerContainer = new ThingOwner<Thing>(this, oneStackOnly: false);
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            compRefuelable = GetComp<CompRefuelable>();
+        }
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            base.DeSpawn(mode);
+            progressBarEffecter?.Cleanup();
+        }
+        public override string Label => base.Label + ": " + Pawn?.LabelShort;
+
+        protected override void Tick()
+        {
+            base.Tick();
+            var pawn = Pawn;
+            if (pawn != null)
+            {
+                if (Spawned)
+                {
+                    var metapodHediff = pawn.health.hediffSet.GetFirstHediff<MetapodHediff>();
+                    metapodHediff.jellyFueled = compRefuelable.HasFuel;
+                    if ((5 * 60) >= metapodHediff.TicksEmergesIn)
+                    {
+                        if (wakeUpEffect == null)
+                        {
+                            wakeUpEffect = InternalDefOf.CocoonWakingUp.SpawnAttached(this, this.Map);
+                        }
+                        wakeUpEffect.EffectTick(this, this);
+                    }
+                    DoProgressBar(metapodHediff.GetProgress());
+                    metapodHediff.Tick();
+                    if (metapodHediff.growthPercent >= 1f)
+                    {
+                        Complete(pawn);
+                    }
+                }
+            }
+            else if (Spawned)
+            {
+                innerContainer.TryDropAll(Position, Map, ThingPlaceMode.Near);
+            }
+        }
+
+        private void Complete(Pawn pawn)
+        {
+            wakeUpEffect?.Cleanup();
+            innerContainer.TryDrop(pawn, ThingPlaceMode.Direct, out _);
+            SpawnInsectStuff(pawn, pawn.Position, pawn.Map);
+            Destroy();
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            foreach (var g in base.GetGizmos())
+            {
+                yield return g;
+            }
+            if (DebugSettings.ShowDevGizmos)
+            {
+                yield return new Command_Action
+                {
+                    defaultLabel = "DEV: Near Complete",
+                    action = () =>
+                    {
+                        var pawn = Pawn;
+                        var metapodHediff = pawn.health.hediffSet.GetFirstHediff<MetapodHediff>();
+                        metapodHediff.growthPercent = 0.99f;
+                    }
+                };
+            }
+        }
+
+        private static readonly IntRange BloodFilth = new IntRange(6, 9);
+
+        public void SpawnInsectStuff(Pawn pawn, IntVec3 pos, Map map)
+        {
+            CellRect cellRect = new CellRect(pos.x, pos.z, 3, 3).ClipInsideMap(map);
+            for (int i = 0; i < BloodFilth.RandomInRange; i++)
+            {
+                IntVec3 randomCell = cellRect.RandomCell;
+                if (randomCell.InBounds(map) && GenSight.LineOfSight(randomCell, pos, map))
+                {
+                    FilthMaker.TryMakeFilth(randomCell, map, ThingDefOf.Filth_BloodInsect);
+                }
+            }
+        }
+
+
+        private void DoProgressBar(float progress)
+        {
+            if (progressBarEffecter == null)
+            {
+                progressBarEffecter = EffecterDefOf.ProgressBar.Spawn();
+            }
+            MoteProgressBar mote = ((SubEffecter_ProgressBar)progressBarEffecter.children[0]).mote;
+            if (mote.DestroyedOrNull())
+            {
+                progressBarEffecter = EffecterDefOf.ProgressBar.Spawn();
+                progressBarEffecter.EffectTick(this, TargetInfo.Invalid);
+                mote = ((SubEffecter_ProgressBar)progressBarEffecter.children[0]).mote;
+            }
+            else
+            {
+                progressBarEffecter.EffectTick(this, TargetInfo.Invalid);
+            }
+            mote.yOffset += 1;
+            mote.progress = progress;
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            base.Destroy(mode);
+            innerContainer.ClearAndDestroyContents();
+        }
+
+        public ThingOwner GetDirectlyHeldThings()
+        {
+            return innerContainer;
+        }
+
+        public void GetChildHolders(List<IThingHolder> outChildren)
+        {
+            ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, GetDirectlyHeldThings());
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Deep.Look(ref innerContainer, "innerContainer", this);
+        }
+
+        public override string GetInspectStringLowPriority()
+        {
+            string text = base.GetInspectStringLowPriority();
+            if (!text.NullOrEmpty())
+            {
+                text += "\n";
+            }
+            var pawn = Pawn;
+            if (pawn != null)
+            {
+                var hediff = pawn.health.hediffSet.GetFirstHediff<MetapodHediff>();
+                var period = hediff.TicksEmergesIn.ToStringTicksToPeriod();
+                text += "VRE_MetapodTimer".Translate(pawn.Named("PAWN"), period);
+            }
+            return text;
+        }
+    }
+}
---
+++ 1.6/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/ScenParts/ScenPart_LowOrbitCrash.cs
@@ -0,0 +1,109 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace VanillaRacesExpandedInsector
+{
+    public class ScenPart_LowOrbitCrash : ScenPart
+    {
+        public override void GenerateIntoMap(Map map)
+        {
+            if (Find.GameInitData == null)
+                return;
+
+           
+            this.DropThingGroupsNear(MapGenerator.PlayerStartSpot, map, CreateThingGroups());
+
+            for (int i = 0; i < 3; i++)
+            {
+                TryFindShipChunkDropCell(InternalDefOf.VRE_InfestedShipPart, MapGenerator.PlayerStartSpot, map, out IntVec3 spawnPos);
+                Thing inner = ThingMaker.MakeThing(InternalDefOf.VRE_InfestedShipPart_Spawned);
+                inner.SetFactionDirect(Faction.OfPlayer);
+
+
+                SkyfallerMaker.SpawnSkyfaller(InternalDefOf.VRE_InfestedShipPart, inner, spawnPos, map);
+
+            }
+        }
+
+
+        private bool TryFindShipChunkDropCell(ThingDef chunk, IntVec3 nearLoc, Map map, out IntVec3 pos) => CellFinderLoose.TryFindSkyfallerCell(chunk, map, TerrainAffordanceDefOf.Walkable, out pos, nearLoc: nearLoc, nearLocMaxDist: 15);
+
+        // Pod utils
+        private List<List<Thing>> CreateThingGroups()
+        {
+            List<List<Thing>> thingsGroups = new();
+            foreach (Pawn pawn in Find.GameInitData.startingAndOptionalPawns)
+                thingsGroups.Add(new List<Thing>() { pawn });
+
+            List<Thing> thingList = new();
+            foreach (ScenPart allPart in Find.Scenario.AllParts)
+                thingList.AddRange(allPart.PlayerStartingThings());
+
+            int index = 0;
+            foreach (Thing thing in thingList)
+            {
+                if (thing.def.CanHaveFaction) thing.SetFactionDirect(Faction.OfPlayer);
+
+                thingsGroups[index].Add(thing);
+
+                ++index;
+                if (index >= thingsGroups.Count) index = 0;
+            }
+
+            return thingsGroups;
+        }
+
+        private void DropThingGroupsNear(IntVec3 dropCenter, Map map, List<List<Thing>> thingsGroups, int openDelay = 110)
+        {
+            foreach (List<Thing> thingsGroup in thingsGroups)
+            {
+                if (!DropCellFinder.TryFindDropSpotNear(dropCenter, map, out IntVec3 result, false, false) && (false || !DropCellFinder.TryFindDropSpotNear(dropCenter, map, out result, false, true)))
+                {
+                    Log.Warning("DropThingsNear failed to find a place to drop " + thingsGroup.FirstOrDefault() + " near " + dropCenter + ". Dropping on random square instead.");
+                    result = CellFinderLoose.RandomCellWith(c => c.Walkable(map), map);
+                }
+
+                for (int index = 0; index < thingsGroup.Count; ++index)
+                    thingsGroup[index].SetForbidden(true, false);
+
+                ActiveTransporterInfo info = new();
+                foreach (Thing thing in thingsGroup)
+                    info.innerContainer.TryAdd(thing);
+                info.openDelay = openDelay;
+                info.leaveSlag = true;
+                this.MakeDropPodAt(result, map, info);
+            }
+        }
+
+        private void MakeDropPodAt(IntVec3 c, Map map, ActiveTransporterInfo info)
+        {
+            ActiveTransporter activeDropPod = (ActiveTransporter)ThingMaker.MakeThing(InternalDefOf.VRE_ActiveDropPod);
+            activeDropPod.Contents = info;
+            Thing inner = ThingMaker.MakeThing(InternalDefOf.VRE_InfestedShipPart_Spawned);
+            activeDropPod.Contents.innerContainer.TryAdd(inner);
+           // activeDropPod.Graphic = inner.Graphic;
+            SkyfallerMaker.SpawnSkyfaller(InternalDefOf.VRE_InfestedShipPart, activeDropPod, c, map);
+            foreach (Thing thing in activeDropPod.Contents.innerContainer)
+            {
+                if (thing is Pawn p1 && p1.IsWorldPawn())
+                {
+                    Find.WorldPawns.RemovePawn(p1);
+                    p1.psychicEntropy?.SetInitialPsyfocusLevel();
+                }
+            }
+        }
+
+        public override void PostMapGenerate(Map map)
+        {
+            if (Find.GameInitData == null)
+                return;
+            PawnUtility.GiveAllStartingPlayerPawnsThought(ThoughtDefOf.CrashedTogether);
+        }
+    }
+}
