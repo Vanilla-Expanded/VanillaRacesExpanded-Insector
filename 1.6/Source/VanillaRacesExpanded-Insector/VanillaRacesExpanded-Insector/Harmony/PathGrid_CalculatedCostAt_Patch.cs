26ace8b3e097144ccc1db5123f3365d2e2253393
SokyranTheDragon <exploder1531@gmail.com>
2025-07-02 18:51:25 +0200

    Fix remaining issues, temporarily disable pathfinding changes

---
+++ 1.6/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/Harmony/PathGrid_CalculatedCostAt_Patch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Verse;
+using Verse.AI;
+
+namespace VanillaRacesExpandedInsector;
+
+[HarmonyPatch(typeof(PathGrid), nameof(PathGrid.CalculatedCostAt))]
+public static class PathGrid_CalculatedCostAt_Patch
+{
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+    {
+        var targetField = typeof(BuildableDef).DeclaredField(nameof(BuildableDef.pathCost));
+        var instrList = instructions.ToList();
+        var patched = 0;
+
+        for (var i = 0; i < instrList.Count; i++)
+        {
+            var instr = instrList[i];
+
+            // num = terrainDef.pathCost
+            // Insert after "terrainDef.pathCost" is pushed to the stack, but before the local value is set using it
+            if (i > 2 && instr.opcode == OpCodes.Stloc_0 && instrList[i - 2].opcode == OpCodes.Ldloc_2 && instrList[i - 1].LoadsField(targetField))
+            {
+                yield return CodeInstruction.Call(typeof(PathGrid_CalculatedCostAt_Patch), nameof(ReplaceValue));
+                patched++;
+            }
+
+            yield return instr;
+        }
+
+        const int expected = 1;
+        if (patched != expected)
+        {
+            var name = (original.DeclaringType?.Namespace).NullOrEmpty() ? original.Name : $"{original.DeclaringType!.Name}:{original.Name}";
+            Log.Warning($"Patched incorrect number of {nameof(PathGrid.CalculatedCostAt)} calls (patched {patched}, expected {expected}) for method {name}");
+        }
+    }
+
+    public static int ReplaceValue(int terrainCost)
+    {
+        if (Pawn_PathFollower_CostToMoveIntoCell_Patch.activePawn != null && terrainCost > 4)
+            terrainCost *= 2;
+        return terrainCost;
+    }
+}
--- 1.6/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/Harmony/Pawn_PathFollower_CostToMoveIntoCell_Patch.cs
+++ 1.6/Source/VanillaRacesExpanded-Insector/VanillaRacesExpanded-Insector/Harmony/Pawn_PathFollower_CostToMoveIntoCell_Patch.cs
@@ -3,22 +3,30 @@ using System;
 using Verse;
 using Verse.AI;
 
-namespace VanillaRacesExpandedInsector
+namespace VanillaRacesExpandedInsector;
+
+[HarmonyPatch(typeof(Pawn_PathFollower), "CostToMoveIntoCell", new Type[] { typeof(Pawn), typeof(IntVec3) })]
+public static class Pawn_PathFollower_CostToMoveIntoCell_Patch
 {
-    [HarmonyPatch(typeof(Pawn_PathFollower), "CostToMoveIntoCell", new Type[] { typeof(Pawn), typeof(IntVec3) })]
-    public static class Pawn_PathFollower_CostToMoveIntoCell_Patch
+    // Rather than using this prefix/finalizer combo with PathGrid.CalculatedCostAt transpiler, we could just
+    // increase the path cost by the terrain cost. However, this would cause several unintended consequences:
+    // - It would increase pathing cost for flying
+    // - It would ignore terrain cost overrides for that pawn
+    // - It would raise cost in situations where terrain cost doesn't apply, like buildings having higher path cost
+    // - Ignore speed boosts from PawnKindDef.moveSpeedFactorByTerrainTag
+    // - Ignore speed changes caused from the job's locomotion urgency
+    // - Possibly raise the path cost over the maximum allowed of 450 (or 900/1350 on lower locomotion urgency)
+
+    public static Pawn activePawn;
+
+    public static void Prefix(Pawn pawn)
     {
-        public static void Postfix(Pawn pawn, IntVec3 c, ref float __result)
-        {
-            if (pawn.Map != null && pawn.HasActiveGene(InternalDefOf.VRE_InflexibleJoints))
-            {
-                var terrain = c.GetTerrain(pawn.Map);
-                if (terrain.pathCost > 4)
-                {
-                    __result += terrain.pathCost;
-                }
-            }
-        }
+        if (pawn.Map != null && pawn.HasActiveGene(InternalDefOf.VRE_InflexibleJoints))
+            activePawn = pawn;
+    }
+
+    public static void Finalizer()
+    {
+        activePawn = null;
     }
 }
-
